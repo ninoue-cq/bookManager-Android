8cc6f8d31efad50c55cee6c07c9dc39c339ef2df
ninoue-cq <ne250068@senshu-u.jp>
2016-07-21 14:12:11 +0900

    アカウント設定画面とログイン画面のレイアウト完成

---
+++ bookManager-Android/app/src/main/java/com/example/inouenaoto/bookmanager_android/AccountSettingActivity.java
@@ -0,0 +1,13 @@
+package com.example.inouenaoto.bookmanager_android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AccountSettingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account_setting);
+    }
+}
