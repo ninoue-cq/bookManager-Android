366502665066edaca81918e5e011e05a382b32f9
ninoue-cq <ne250068@senshu-u.jp>
2016-07-28 13:31:49 +0900

    追加画面で未入力項目があるときのダイアログの追加

--- bookManager-Android/app/src/main/java/com/example/inouenaoto/bookmanager_android/BookAddActivity.java
+++ bookManager-Android/app/src/main/java/com/example/inouenaoto/bookmanager_android/BookAddActivity.java
@@ -95,19 +95,31 @@ public class BookAddActivity extends Activity {
                 finish();
                 break;
             case R.id.save_button:
-                //書籍追加の処理 
+                //書籍追加の処理 長いのでメソッドにしたほうがいいのかもしれない
                 EditText bookTitle = (EditText) findViewById(R.id.add_book_title);
                 final String addBookTitle = bookTitle.getText().toString();
 
                 EditText bookPrice = (EditText) findViewById(R.id.add_book_price);
-                final String addPriceText = bookPrice.getText().toString();
-
+                final String addBookPrice = bookPrice.getText().toString();
                 EditText bookDate = (EditText) findViewById(R.id.add_book_date);
-                final String addDateText = bookDate.getText().toString();
-                new BookDataAdd().execute(addBookTitle, addPriceText, addDateText);
-                Intent intent = new Intent(BookAddActivity.this, MainActivity.class);
-                startActivity(intent);
-                break;
+                final String addBookDate = bookDate.getText().toString();
+
+                if (addBookTitle.length() == 0 || addBookPrice.length() == 0 || addBookDate.length() == 0) {
+                    AlertDialog.Builder alertDialog = new AlertDialog.Builder(BookAddActivity.this);
+                    alertDialog.setMessage("未入力項目があります");
+                    alertDialog.setPositiveButton("再入力",
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {
+
+                                }
+                            });
+                    alertDialog.show();
+                } else {
+                    new BookDataAdd().execute(addBookTitle, addBookPrice, addBookDate);
+                    Intent intent = new Intent(BookAddActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    break;
+                }
         }
         return true;
     }
